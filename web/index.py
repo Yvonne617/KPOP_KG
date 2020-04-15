3dacbe60df98cf9d859eca18b60a7dcb644dae03
Yvonne617 <chen238@usc.edu>
2020-04-15 10:16:04 -0700

    web

---
+++ web/index.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template
+from flask import request
+import rltk
+import rdflib 
+from flask import jsonify 
+app = Flask(__name__,template_folder='templates')
+prefix =  """ 
+        PREFIX : <undefined>
+        PREFIX kpop: <http://inf558.org/kpop/>
+        PREFIX dbr: <http://dbpedia.org/resource/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX fanm: <https://kpop.fandom.com/wiki/>  """
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/groups')
+def groups():
+    keyword = request.args.get("keyword")
+    g = rdflib.Graph()
+    result = g.parse('data/KPOP_graph.ttl', format='n3')
+    qres = g.query("""
+    SELECT ?name WHERE { 
+    ?group a schema:Class .
+	?group rdfs:label ?name .
+    FILTER regex(?name,"EXO", "i") 
+    }
+     """)
+    for row in qres:
+        print(row)
+    return jsonify({"data": keyword})
+@app.route('/members')
+def members():
+    keyword = request.args.get("keyword")
+    return jsonify({"data": keyword})
