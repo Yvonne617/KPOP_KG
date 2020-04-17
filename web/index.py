40771cd6d5d3ad1711625096384bd97982952381
Yvonne617 <chen238@usc.edu>
2020-04-16 21:36:42 -0700

    data update

--- web/index.py
+++ web/index.py
@@ -23,7 +23,7 @@ prefix =  """
         PREFIX owl: <http://www.w3.org/2002/07/owl#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX fanm: <https://kpop.fandom.com/wiki/> 
+        PREFIX fadm: <https://kpop.fandom.com/wiki/> 
         PREFIX schema: <http://schema.org/> """
 @app.route('/')
 def index():
